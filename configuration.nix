85d8377fa7675502f143a63e0cdfae4119612326
Jozef833 <172046463+Jozef833@users.noreply.github.com>
2025-07-17 15:37:27 +0000

    Home Manager

--- configuration.nix
+++ configuration.nix
@@ -14,28 +14,24 @@
     localBinInPath = true; # For uv
 
     shellInit = ''
-      git config --global init.defaultBranch "main"
-      git config --global user.email "172046463+Jozef833@users.noreply.github.com"
-      git config --global user.name "Jozef833"
       if ! pgrep -u $USER ssh-agent > /dev/null; then
         eval "$(ssh-agent -s)"
-        ssh-add /home/${username}/.ssh/id_ed25519
-        ssh-add /home/${username}/.ssh/id_rsa
+        ssh-add ~/.ssh/id_ed25519
+        ssh-add ~/.ssh/id_rsa
       fi
     '';
 
     systemPackages = with pkgs; [
       iamb
-      git
       nodejs_20
       python312
       tree
       uv
 
-      (pkgs.writeShellScriptBin "python" ''
-        export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-        exec ${pkgs.python312}/bin/python "$@"
-      '')
+      #(pkgs.writeShellScriptBin "python" ''
+      #  export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+      #  exec ${pkgs.python312}/bin/python "$@"
+      #'')
     ];
 
     variables = {
@@ -56,32 +52,23 @@
   nixpkgs.config.allowUnfree = true;
 
   programs = {
-    neovim = {
-      configure = {
-        customRC = ''
-          au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
-          set autoindent
-          set expandtab
-          set mouse=a
-          set number
-          set relativenumber
-          set shiftwidth=2
-          set smartindent
-          set smarttab
-          set tabstop=2
-          syntax on
-        '';
-      };
-      defaultEditor = true;
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-    };
-
     nix-ld = {
       enable = true;
       libraries = with pkgs; [
-        zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
+        acl
+        attr
+        bzip2
+        curl
+        libsodium
+        libssh
+        libxml2
+        openssl
+        stdenv.cc.cc
+        systemd
+        util-linux
+        xz
+        zlib
+        zstd
       ];
     };
   };
@@ -89,8 +76,8 @@
   system.stateVersion = stateVersion;
 
   wsl = {
-    defaultUser = username;
     enable = true;
+    defaultUser = username;
     wslConf.network.generateResolvConf = false;
   };
 }
--- home.nix
+++ home.nix
@@ -23,4 +23,35 @@
     stateVersion = stateVersion;
     username = username;
   };
+
+  programs = {
+    git = {
+      enable = true;
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+      userEmail = "Jozef.Porubcin@onmilliman.com";
+      userName = "Jozef Porubcin";
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      extraConfig = ''
+        au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+        set autoindent
+        set expandtab
+        set mouse=a
+        set number
+        set relativenumber
+        set shiftwidth=2
+        set smartindent
+        set smarttab
+        set tabstop=2
+        syntax on
+      '';
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
 }
